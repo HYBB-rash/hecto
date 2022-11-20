da24c52dcf6731347ecf9eb98db9824fe79f64b7
Herman Hong <hyong_cs@outlook.com>
2022-11-21 01:09:27 +0800

    first init

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+mod editor;
+mod terminal;
+mod document;
+mod row;
+
+use editor::Editor;
+pub use editor::Position;
+pub use row::Row;
+pub use document::Document;
+pub use terminal::Terminal;
+
+fn main() {
+    Editor::default().run();
+}
